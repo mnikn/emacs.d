f50315a9b259c6d699947d81913dac20e8f19261
mnikn <329121948@qq.com>
2019-08-12 22:55:38 +0800

    update package

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,5 +1,16 @@
+;;; init-company.el --- Company config -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 (require-package 'company)
 
+(setq-default company-dabbrev-downcase 0)
+(setq-default company-idle-delay 0)
+(setq-default company-minimum-prefix-length 1)
+(setq-default company-dabbrev-code-everywhere t)
+(setq-default company-dabbrev-code-ignore-case t)
 (add-hook 'after-init-hook 'global-company-mode)
 
 (provide 'init-company)
+
+;;; init-company.el ends here
--- lisp/init-langs.el
+++ lisp/init-langs.el
@@ -2,7 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
+;; webmode
+(defun my-web-mode-hook ()
+  "Custom web-mode hook."
 (require-package 'web-mode)
+(setq-default web-mode-markup-indent-offset 2)
+(setq-default web-mode-css-indent-offset 2)
+(setq-default web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
@@ -11,9 +18,29 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 
 ;; javascript
+;(defun my/use-eslint-from-node-modules ()
+;	"Use eslint from local."
+;  (let* ((root (locate-dominating-file
+;                (or (buffer-file-name) default-directory)
+;                "node_modules"))
+;         (eslint (and root
+;                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+;                                        root))))
+;    (when (and eslint (file-executable-p eslint))
+;      (setq-local flycheck-javascript-eslint-executable eslint))))
+;(setq-default flycheck-disabled-checkers
+;							(append flycheck-disabled-checkers '(javascript-jshint)))
+;(flycheck-add-mode 'javascript-eslint 'web-mode)
+																				;(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 (require-package 'js2-mode)
+(setq-default js2-basic-offset 2)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
+(add-hook 'js-mode-hook 'js2-minor-mode)
+
 
 ;; typescript
 (defun setup-tide-mode ()
@@ -21,12 +48,12 @@
   (interactive)
   (tide-setup)
   (flycheck-mode t)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (setq-default flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode t)
   (company-mode t))
 
 (require-package 'tide)
-(setq company-tooltip-align-annotations t)
+(setq-default company-tooltip-align-annotations t)
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 (add-hook 'before-save-hook 'tide-format-before-save)
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
@@ -36,8 +63,6 @@
               (setup-tide-mode))))
 ;; enable typescript-tslint checker
 (flycheck-add-mode 'typescript-tslint 'web-mode)
-(setq tide-format-options
-      '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunction t :placeOpenBraceOnNewLineForControlBlocks t :indentSize 2 :tabSize 2))
 
 (provide 'init-langs)
 
