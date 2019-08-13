1c5eed3456ce45f503c59638b04a37b0b157455a
mnikn <329121948@qq.com>
2019-08-13 22:57:24 +0800

    add package

--- init.el
+++ init.el
@@ -34,16 +34,24 @@
 (require 'init-langs)
 (require 'init-ctags)
 (require 'init-evil)
+
 ;; (require 'init-smex)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-auto-complete t)
+ '(company-dabbrev-downcase 2)
+ '(company-dabbrev-minimum-length 4)
+ '(company-idle-delay 0)
+ '(company-minimum-prefix-length 1)
+ '(company-show-numbers t)
  '(package-selected-packages
-   (quote
-    (tide js2-mode counsel-projectile treemacs-icons-dired treemacs-magit treemacs-projectile treemacs-evil treemacs projectile magit general evil smiles-mode smex))))
+	 (quote
+		(company-quickhelp company-help-mode tide js2-mode counsel-projectile treemacs-icons-dired treemacs-magit treemacs-projectile treemacs-evil treemacs projectile magit general evil smiles-mode smex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-company.el
+++ lisp/init-company.el
@@ -3,13 +3,9 @@
 ;;; Code:
 
 (require-package 'company)
-
-(setq-default company-dabbrev-downcase 0)
-(setq-default company-idle-delay 0)
-(setq-default company-minimum-prefix-length 1)
-(setq-default company-dabbrev-code-everywhere t)
-(setq-default company-dabbrev-code-ignore-case t)
+(require-package 'company-quickhelp)
 (add-hook 'after-init-hook 'global-company-mode)
+(company-quickhelp-mode)
 
 (provide 'init-company)
 
