8d32bbe2cb2b8c86d72d5faec1ab986d1826ba33
mnikn <329121948@qq.com>
2019-08-15 23:02:46 +0800

    add init bash

---
+++ init.sh
@@ -0,0 +1,11 @@
+# for format-all-code 
+npm install --global prettier
+
+# for language servers
+## js/ts
+npm i -g typescript-language-server
+npm i -g typescript
+## python
+pip install ‘python-language-server[all]’	
+## bash
+npm i -g bash-language-server	
