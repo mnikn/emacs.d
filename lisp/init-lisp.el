01ca8fc73b8558d048a695e9b1e95878ef4bcb2f
mnikn <329121948@qq.com>
2019-08-19 22:02:22 +0800

    update paren

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -2,6 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
+(define-advice show-paren-function (:around (fn) fix-show-paren-function)
+  "Highlight parens"
+  (cond ((looking-at-p "\\s(") (funcall fn))
+	(t (save-excursion
+	     (ignore-errors (backward-up-list))
+	     (funcall fn)))))
+
 (electric-pair-mode t)
 (set-face-background 'show-paren-match (face-background 'default))
 (set-face-foreground 'show-paren-match "#03a1fc")
