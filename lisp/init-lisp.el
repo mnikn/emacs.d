7fd90d9c0920f900688b05fb5bc049b907a2238b
mnikn <329121948@qq.com>
2019-09-04 06:52:12 +0800

    update

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -13,7 +13,7 @@
 (set-face-background 'show-paren-match (face-background 'default))
 (set-face-foreground 'show-paren-match "#03a1fc")
 (set-face-attribute 'show-paren-match nil :weight 'extra-bold)
-(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+(add-hook 'prog-mode-hook 'show-paren-mode)
 
 (provide 'init-lisp)
 
